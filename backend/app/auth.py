0189248c21bf619539774926855e78e7aa846e6c
Rahul-Kumar-Parida <rahulkumarparida56@gmail.com>
2025-07-20 17:23:01 +0530

    add

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -19,6 +19,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/login")
 def get_password_hash(password):
     return pwd_context.hash(password)
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
